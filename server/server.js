b2ac6532625601e785509a32d3a62cbf184d6bf5
Florian Streckenbach <florian.streckenbach@MacBook-Pro.fritz.box>
2019-02-14 15:01:05 +0100

    POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,42 @@
+
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {User} = require('./models/user');
+let {Todo} = require('./models/todo');
+
+let app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.post('/todos',(req, res)=>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+
+});
+
+
+app.listen(3000,()=>{
+  console.log('server startet on p 3000');
+})
+
+//
+// let newUser = new usr({
+//    email: '      mail@florkan.net'
+// })
+//
+// newUser.save().then((doc)=>{
+//   console.log(`saved new user: \n ${doc}`);
+// },(e)=>{
+//   console.log(`cant save user because\n ${e}` );
+// })
